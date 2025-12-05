57065a5aa92c785af4f363b70bcf2e3c85881008
Mikel <mikel.san@educa.madrid.org>
2025-12-05 12:33:54 +0100

    airline

---
+++ .metals/.reports/metals-full/2025-12-05/r_empty-definition_10-38-33-444.md
@@ -0,0 +1,26 @@
+error id: file://<WORKSPACE>/src/main/scala/com/aideclare/airline/Customer.scala:`<none>`.
+file://<WORKSPACE>/src/main/scala/com/aideclare/airline/Customer.scala
+empty definition using pc, found symbol in pc: `<none>`.
+empty definition using semanticdb
+empty definition using fallback
+non-local guesses:
+	 -id#
+	 -scala/Predef.id#
+offset: 67
+uri: file://<WORKSPACE>/src/main/scala/com/aideclare/airline/Customer.scala
+text:
+```scala
+package com.aideclare.airline
+
+
+
+final case class Customer(
+    @id@@ iden: Long
+) derives EntityMetadata
+
+```
+
+
+#### Short summary: 
+
+empty definition using pc, found symbol in pc: `<none>`.
---
+++ src/main/scala/com/aideclare/airline/AirlineApp.scala
@@ -0,0 +1,37 @@
+package com.aideclare.airline
+
+import com.aideclare.entities.dsl.adt.Endpoint
+import com.aideclare.entities.dsl.syntax.*
+
+class AirlineApp[System, Api[_]](using systemDsl: SystemDsl[System, Api], apiDsl: ApiDsl[Api]):
+
+  import apiDsl.*
+  import com.aideclare.entities.dsl.syntax.*
+  import systemDsl.*
+
+  def airlineSystem: System =
+    // Rest system name declaration
+    system("airline")
+      .public[Company](
+        api[Company](Endpoint.ReadResourceEndpoints)./[Flight](
+            api(Endpoint.ReadResourceEndpoints)
+        )
+      )
+      .public[Flight](api[Flight](Endpoint.ReadResourceEndpoints))
+      .me[CompanyAdmin](
+        api[CompanyAdmin]().->[Company](
+            api[Company]()./[Flight](
+                api[Flight]()./[Passenger](api[Passenger]())
+            )
+        )
+      )
+      .me[Customer](
+        api[Customer]()./[Passenger](
+            api[Passenger]().->[Flight](
+                api[Flight]()./[Passenger](api[Passenger]())
+            )
+        )
+      )
+      .authenticated[Customer, CompanyAdmin]
+      
+    
